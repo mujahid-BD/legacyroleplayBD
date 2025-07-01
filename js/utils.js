3a28cbf384cbb2102bf4f71fb5f1f7cce24a1f48
medishop-BD <hasangonipurtraders@gmail.com>
2025-07-01 17:54:14 +0600

    Update utils.js

---
+++ js/utils.js
@@ -0,0 +1,14 @@
+// js/utils.js
+function loadLayout() {
+  fetch('header.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+    });
+
+  fetch('footer.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    });
+}
